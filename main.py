68355fdf05ce9b3b8058c980a16ba74041c20d30
devonjfox <78319259+devonjfox@users.noreply.github.com>
2021-05-19 19:22:19 -0400

    splitting columns

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+import sys
 import pandas as pd
 
-input_file_path = "SampleFile.csv"
-export_file_name = "SplitColumn.csv"
-input_file_column_name = "Full Name"
+#input_file_path = "SampleFile.xlsx"
+export_file_name = "SplitColumn.xlsx"
+input_file_column_name = "name"
 
 
 if __name__ == '__main__':
-    df = pd.read_csv(input_file_path)
-    df = df[input_file_column_name].str.split(expand=True)
-    df.to_csv(export_file_name, index=False)
+    print("Split Column program")
+    input_file_path=sys.argv[1]
+    df = pd.read_excel(input_file_path)
+    df = df[input_file_column_name].str.split(expand=True, pat=',')
+    df = df.apply(lambda x: x.str.capitalize())
+    df = pd.DataFrame(data={"First":df[1], "Last":df[0]})
+    df.to_excel(export_file_name, index=False)
+    print("Ending Split Column Program")
